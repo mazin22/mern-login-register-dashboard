3bedf2e79e0f5072655bd1cdf37dadf46bc0f4af
mazin22 <44357016+mazin22@users.noreply.github.com>
2024-01-02 12:59:54 +0530

    index.js

--- server/index.js
+++ server/index.js
@@ -7,9 +7,12 @@ const cors = require('cors');
 app.use(express.json());
 app.use(cors());
 
+var port = process.env.PORT;
+
 // Let us run the server. So its running
-const port = 8000
-app.listen(port, () => console.log(`Server is running on port ${port}`))
+app.listen(port, () => {
+    console.log(`Server is listening on ${port}`);
+});
 
 // Let us create our database (mysql)
 const db = mysql.createConnection({
